af53ba1888a189adb8ce11f23cd72aac74e44ff1
Landerer0 <lucaskraemera@gmail.com>
2022-11-21 22:31:28 -0300

    eliminacion de PCSA

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 Se desea verificar la cantidad de espacio reducido al utilizar distintas estructuras en el contexto de HyperLogLog.
 
 ## Compilación
-Compilar fichero con: g++ -std=c++11 -O3 -DNDEBUG -I ~/include -L ~/lib program.cpp -o proyecto -lsdsl -ldivsufsort -ldivsufsort64
+Compilar fichero con: 
+g++ -std=c++11 -O3 -DNDEBUG -I ~/include -L ~/lib *.cpp -o proyecto -lsdsl -ldivsufsort -ldivsufsort64 -fopenmp -Wall
 
 ## Ejecución
 ./proyecto [nombre_del_archivo_1] [nombre_del_archivo_2]
--- main.cpp
+++ main.cpp
@@ -10,7 +10,6 @@
 #include <chrono>
 #include <omp.h>
 
-#include "pcsa.hpp"
 #include "Hyperloglog.hpp"
 
 using namespace std;
@@ -21,7 +20,7 @@ const unsigned int k = 31; //tamaño del kmer
 
 const int numThreads = 5; // 80-31 = 49, 49%7=0 division exacta
 
-void lectura(Hyperloglog &hll, PCSA &pcsa, ifstream &file){
+void lectura(Hyperloglog &hll, ifstream &file){
   int linea = 0;
   omp_set_num_threads(numThreads);  
   ull progreso = 0;
@@ -37,7 +36,6 @@ void lectura(Hyperloglog &hll, PCSA &pcsa, ifstream &file){
     for(int i = 0; i <= line.size() - k; i++){
       string aux = line.substr(i,k);  
       hll.update(aux);
-      pcsa.update(aux);
     }
   }
 
@@ -67,40 +65,30 @@ template <typename T> T readStream(unordered_set<string> &gt, ifstream &file, un
   return estimator;
 }
 
-vector<long long> operations(Hyperloglog &hll1, Hyperloglog &hll2, PCSA &pcsa1, PCSA &pcsa2){
+vector<long long> operations(Hyperloglog &hll1, Hyperloglog &hll2){
   vector<long long> resultado;
   // se crean estructuras vacias para guardar la union en ellas
   Hyperloglog hllMerge(Buckets);
-  PCSA pcsaMerge(Buckets);
   hllMerge.merge(hll1);
   hllMerge.merge(hll2);
-  pcsaMerge.merge(pcsa1);
-  pcsaMerge.merge(pcsa2);
   // estimaciones de los ficheros
-  long long hllEstimate1,hllEstimate2,pcsaEstimate1,pcsaEstimate2;
-  long long hllMergeEstimate,pcsaMergeEstimate;
-  long long hllIntersectionEstimate, pcsaIntersectionEstimate;
-  long long hllSetDifference1, hllSetDifference2, pcsaSetDifference1, pcsaSetDifference2;
-  long long hllSymmetricDifference, pcsaSymmetricDifference;
-  long long hllJaccard, pcsaJaccard;
+  long long hllEstimate1,hllEstimate2;
+  long long hllMergeEstimate;
+  long long hllIntersectionEstimate;
+  long long hllSetDifference1, hllSetDifference2;
+  long long hllSymmetricDifference;
+  long long hllJaccard;
 
   hllEstimate1 = hll1.estimate(); 
   hllEstimate2 = hll2.estimate(); 
-  pcsaEstimate1 = pcsa1.estimate(); 
-  pcsaEstimate2 = pcsa2.estimate(); 
   hllMergeEstimate = hllMerge.estimate();
-  pcsaMergeEstimate = pcsaMerge.estimate();
   // |A ∩ B| = |A| + |B| - |A U B|
   hllIntersectionEstimate = hllEstimate1 + hllEstimate2 - hllMergeEstimate;
-  pcsaIntersectionEstimate = pcsaEstimate1 + pcsaEstimate2 - pcsaMergeEstimate;
   // |A – B| = |A| - |A ∩ B|
   hllSetDifference1 = hllEstimate1 - hllIntersectionEstimate;
   hllSetDifference2 = hllEstimate2 - hllIntersectionEstimate;
-  pcsaSetDifference1 = pcsaEstimate1 - pcsaIntersectionEstimate;
-  pcsaSetDifference2 = pcsaEstimate2 - pcsaIntersectionEstimate;
   // |A △ B| = |A| + |B| - |A ∩ B|
   hllSymmetricDifference = hllMergeEstimate - hllIntersectionEstimate;
-  pcsaSymmetricDifference = pcsaMergeEstimate - pcsaIntersectionEstimate;
 
   // HyperLogLog
   resultado.push_back(hllEstimate1); // 0
@@ -110,14 +98,6 @@ vector<long long> operations(Hyperloglog &hll1, Hyperloglog &hll2, PCSA &pcsa1, 
   resultado.push_back(hllSetDifference1); // 4
   resultado.push_back(hllSetDifference2); // 5
   resultado.push_back(hllSymmetricDifference); // 6
-  // PCSA
-  resultado.push_back(pcsaEstimate1); // 7
-  resultado.push_back(pcsaEstimate2); // 8
-  resultado.push_back(pcsaMergeEstimate); // 9
-  resultado.push_back(pcsaIntersectionEstimate); // 10
-  resultado.push_back(pcsaSetDifference1); // 11
-  resultado.push_back(pcsaSetDifference2); // 12
-  resultado.push_back(pcsaSymmetricDifference); // 13
 
   return resultado;
 }
@@ -130,14 +110,12 @@ int main(int argc, char *argv[]) {
     auto start = high_resolution_clock::now();
     if(argc < 4){
       Hyperloglog hll1(Buckets);
-      PCSA pcsa1(Buckets);
       Hyperloglog hll2(Buckets);
-      PCSA pcsa2(Buckets);
-      lectura(hll1,pcsa1,file1); // lectura del primer archivo
-      lectura(hll2,pcsa2,file2); // lectura del segundo archivo
+      lectura(hll1,file1); // lectura del primer archivo
+      lectura(hll2,file2); // lectura del segundo archivo
       
       // calculo de estadisticas de las estructuras
-      vector<long long> result = operations(hll1,hll2,pcsa1,pcsa2) ;
+      vector<long long> result = operations(hll1,hll2) ;
 
       cout << "Estimacion HLL: "  << result.at(0)  << " " << result.at(1) << endl;
       cout << "Merge HLL: " << result.at(2) << endl;
@@ -145,13 +123,6 @@ int main(int argc, char *argv[]) {
       cout << "Set Difference HLL: '1' " << result.at(4) << " '2' " << result.at(5) << endl;
       cout << "Symmetric Difference HLL: " << result.at(6) << endl;
       cout << "Jaccard HLL: " << ((double)result.at(3)/(double)result.at(2)) << endl;
-
-      cout << "Estimacion PCSA: "  << result.at(7)  << " " << result.at(8) << endl;
-      cout << "Merge PCSA: " << result.at(9) << endl;
-      cout << "Intersection PCSA: " << result.at(10) << endl;
-      cout << "Set Difference PCSA: '1' " << result.at(11) << " '2' " << result.at(12) << endl;
-      cout << "Symmetric Difference PCSA: " << result.at(13) << endl;
-      cout << "Jaccard PCSA: " << ((double)result.at(10)/(double)result.at(9)) << endl;
     }
     auto stop = high_resolution_clock::now();
     auto duration = duration_cast<milliseconds>(stop - start);
